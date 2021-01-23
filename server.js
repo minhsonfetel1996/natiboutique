0b0ec743392ea704b25ac15e8b745bce70beeba1
smpham <son.minh.pham@mgm-tp.com>
2021-01-23 12:07:32 +0700

    fixed bug dir

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (
   const express = require("express");
   const path = require("path");
 
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
